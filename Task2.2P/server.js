bb426c0c664e8a970063b5b4b3b7c948d2dc05d8
ronohbar <ronohbarimbar@gmail.com>
2025-01-15 00:36:48 +1100

    added other calculations

--- Task2.2P/server.js
+++ Task2.2P/server.js
@@ -9,6 +9,27 @@ app.get("/addTwoNumbers/:num1/:num2",function (req,res) {
     return res.json({statusCode:200,result}).status(200)
 })
 
+app.get("/subtractTwoNumbers/:num1/:num2",function (req,res) {
+    const num1 = parseInt(req.params.num1)
+    const num2 = parseInt(req.params.num2)
+    const result = num1-num2    
+    return res.json({statusCode:200,result}).status(200)
+})
+
+app.get("/multiplyTwoNumbers/:num1/:num2",function (req,res) {
+    const num1 = parseInt(req.params.num1)
+    const num2 = parseInt(req.params.num2)
+    const result = num1*num2    
+    return res.json({statusCode:200,result}).status(200)
+})
+
+app.get("divideTwoNumbers/:num1/:num2",function (req,res) {
+    const num1 = parseInt(req.params.num1)
+    const num2 = parseInt(req.params.num2)
+    const result = num1/num2    
+    return res.json({statusCode:200,result}).status(200)
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, function () {
     console.log("App listening on port " + port);
